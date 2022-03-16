8198a85f596c498d7d768dddc71ac3e6e3ad7890
PedroMarineli <phmo.1606@gmail.com>
2022-03-16 17:16:03 +0000

    ff

---
+++ main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args) {
+        int numeros[];
+        numeros = new int[10];
+
+        int novos[] = new int[10];
+
+        String frutas[] = {"Laranja", "Abacaxi", "Uva"};
+    }
+}
